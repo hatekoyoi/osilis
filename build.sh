4a36e57e92fc0dfb150a21c863d41a3b55b45289
ky0he1 <kyouta1218@gmail.com>
2023-04-09 20:11:05 +0900

    ブートローダとカーネルを実装

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+cd $HOME/github/osilis/kernel
+clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp
+ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -z separate-code -o kernel.elf main.o
+
 cd $HOME/edk2
 source edksetup.sh
 build
-$HOME/osbook/devenv/run_qemu.sh Build/OsilisLoaderX64/DEBUG_CLANG38/X64/Loader.efi
+$HOME/osbook/devenv/run_qemu.sh Build/OsilisLoaderX64/DEBUG_CLANG38/X64/Loader.efi $HOME/github/osilis/kernel/kernel.elf
