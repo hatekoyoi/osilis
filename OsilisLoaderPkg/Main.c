dd148644c496a2ee469f4801cc814c7931a82931
ky0he1 <kyouta1218@gmail.com>
2023-04-12 20:26:47 +0900

    コメント追記

--- OsilisLoaderPkg/Main.c
+++ OsilisLoaderPkg/Main.c
@@ -141,12 +141,13 @@ OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
 
 EFI_STATUS
 OpenGOP(EFI_HANDLE image_handle, EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
+    // Graphics Output Protocol(GOP)のハンドルを取得
     UINTN num_gop_handles = 0;
     EFI_HANDLE* gop_handles = NULL;
-
     gBS->LocateHandleBuffer(
         ByProtocol, &gEfiGraphicsOutputProtocolGuid, NULL, &num_gop_handles, &gop_handles);
 
+    // GOPのハンドルを使用して、グラフィックス出力プロトコルを開く
     gBS->OpenProtocol(gop_handles[0],
                       &gEfiGraphicsOutputProtocolGuid,
                       (VOID**)gop,
@@ -154,11 +155,13 @@ OpenGOP(EFI_HANDLE image_handle, EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
                       NULL,
                       EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
+    // バッファ開放
     FreePool(gop_handles);
 
     return EFI_SUCCESS;
 }
 
+// ピクセルフォーマットの値に応じて、Unicode文字列を返す
 const CHAR16*
 GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
     switch (fmt) {
@@ -207,46 +210,54 @@ UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
 
     EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
     OpenGOP(image_handle, &gop);
+    // GOP モードの情報を表示する
     Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
           gop->Mode->Info->HorizontalResolution,
           gop->Mode->Info->VerticalResolution,
           GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
           gop->Mode->Info->PixelsPerScanLine);
+    // フレームバッファの情報を表示する
     Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
           gop->Mode->FrameBufferBase,
           gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
           gop->Mode->FrameBufferSize);
 
+    // フレームバッファを白色で塗りつぶす
     UINT8* frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
     for (UINTN i = 0; i < gop->Mode->FrameBufferSize; i++) {
         frame_buffer[i] = 255;
     }
 
+    // kernelを開く
     EFI_FILE_PROTOCOL* kernel_file;
     root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
 
+    // kernelのサイズを取得する
     UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
     UINT8 file_info_buffer[file_info_size];
     kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
-
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
+    // kernelの読み込みとメモリへの配置
     EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
     gBS->AllocatePages(
         AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
     kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
     Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
+    // メモリマップの取得とBoot終了
     EFI_STATUS status;
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status)) {
+        // メモリマップの取得に失敗した場合はエラーを表示して停止
         status = GetMemoryMap(&memmap);
         if (EFI_ERROR(status)) {
             Print(L"failed to get memory map: %r\n", status);
             while (1)
                 ;
         }
+        // Bootの終了を再度試みる
         status = gBS->ExitBootServices(image_handle, memmap.map_key);
         if (EFI_ERROR(status)) {
             Print(L"Could not exit boot service: %r\n", status);
@@ -255,8 +266,8 @@ UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
         }
     }
 
+    // kernelのエントリーポイントを実行する
     UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
-
     typedef void EntryPointType(UINT64, UINT64);
     EntryPointType* entry_point = (EntryPointType*)entry_addr;
     entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,10 +2,14 @@
 
 extern "C" void
 KernelMain(uint64_t frame_buffer_base, uint64_t frame_buffer_size) {
+    // フレームバッファのアドレスを uint8_t ポインタにキャスト
     uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+
+    // フレームバッファに色を付ける
     for (uint64_t i = 0; i < frame_buffer_size; i++) {
         frame_buffer[i] = i % 256;
     }
+
     while (1)
         __asm__("hlt");
 }
